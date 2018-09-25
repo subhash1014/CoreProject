fad6e6f8bea65933f463206a716a6b6f20199ece
Subramaniam <subhash.subramaniam@misys.com>
2018-09-25 17:56:18 +0530

    second commit

--- src/com/rsv/appmain/ApplicationMain.java
+++ src/com/rsv/appmain/ApplicationMain.java
@@ -2,7 +2,7 @@ package com.rsv.appmain;
 public class ApplicationMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Rest Api branch");
 
 	}
 
