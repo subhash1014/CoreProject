bd1a9f4d190ba1d4398254494f9999156256cb97
Subramaniam <subhash.subramaniam@misys.com>
2018-10-04 11:42:24 +0530

    New branch

--- src/com/rsv/appmain/ApplicationMain.java
+++ src/com/rsv/appmain/ApplicationMain.java
@@ -2,13 +2,7 @@ package com.rsv.appmain;
 public class ApplicationMain {
 
 	public static void main(String[] args) {
-		System.out.println("Rest Api branch");
-		System.out.println("Changes from anthor workspace");
-		System.out.println("Changes from anthor workspace added new changes");
-		System.out.println("Changes from anthor workspace added new changes two");
-		System.out.println("Changes from anthor workspace added new changes three");
-		System.out.println("Changes amend");
-		System.out.println("Changes amend2");
+		System.out.println("New Rest Api branch");
 
 	}
 
